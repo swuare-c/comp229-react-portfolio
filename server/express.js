598b755e3c672ad6865360e5e598b9c99f485726
christian <christiankhan100@gmail.com>
2025-07-30 03:29:43 -0400

    Fixed express.js

--- server/express.js
+++ server/express.js
@@ -3,19 +3,23 @@ import cookieParser from "cookie-parser";
 import compress from "compression";
 import cors from "cors";
 import helmet from "helmet";
+import path from "path";
 
 const app = express();
+
+const CURRENT_WORKING_DIR = process.cwd();
 
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true
 }));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(compress());
 app.use(helmet());
+
+app.use(express.static(path.join(CURRENT_WORKING_DIR, "dist", "app")));
 
 app.use((err, req, res, next) => {
   if (err.name === "UnauthorizedError") {
